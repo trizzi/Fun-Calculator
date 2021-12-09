11a02880d4b58712fcd16761ffd4fa746cfb020e
trizzi <patrickakhamiogu@gmail.comdquote>
2021-12-09 15:32:40 +0100

    Complete javascript code

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 let runningTotal = 0;
-let buffer = '0s';
+let typedValue = '0';
+let previousOperator = null;
 
 const screen = document.querySelector('.screen');
-const button = document.querySelector('.cal-button');
+const button = document.querySelector('.calc-buttons');
 
 button.addEventListener('click', function (e) {
-  e.preventDefault();
   buttonClick(e.target.innerText);
-  console.log(
-    'Element clicked through function!',
-    e.target.innerText
-  );
 });
 
 const buttonClick = (value) => {
@@ -19,14 +15,73 @@ const buttonClick = (value) => {
   } else {
     handleNumber(value);
   }
+  rerender();
 };
 
-const handleSymbol = (value) => {};
+const handleSymbol = (value) => {
+  switch (value) {
+    case 'C':
+      typedValue = '0';
+      runningTotal = 0;
+      previousOperator = null;
+      break;
+    case '=':
+      if (previousOperator === null) {
+        return;
+      } else {
+        flushOperation(parseInt(typedValue));
+        previousOperator = null;
+        typedValue = '' + runningTotal;
+        runningTotal = 0;
+      }
+      break;
+    case '←':
+      if (typedValue.length === 1) {
+        typedValue = '0';
+      } else {
+        typedValue = typedValue.substring(
+          0,
+          typedValue.length - 1
+        );
+      }
+      break;
+    default:
+      handleMath(value);
+      break;
+  }
+};
 
 const handleNumber = (value) => {
-  if (buffer === '0') {
-    buffer = value;
+  if (typedValue === '0') {
+    typedValue = value;
   } else {
-    buffer += value;
+    typedValue += value;
   }
+};
+
+const handleMath = (value) => {
+  const intTypedvalue = parseInt(typedValue);
+  if (runningTotal === 0) {
+    runningTotal = intTypedvalue;
+  } else {
+    flushOperation(intTypedvalue);
+  }
+  previousOperator = value;
+  typedValue = '0';
+};
+
+const flushOperation = (intTypedvalue) => {
+  if (previousOperator === '+') {
+    runningTotal += intTypedvalue;
+  } else if (previousOperator === '-') {
+    runningTotal -= intTypedvalue;
+  } else if (previousOperator === 'X') {
+    runningTotal *= intTypedvalue;
+  } else {
+    runningTotal /= intTypedvalue;
+  }
+};
+
+const rerender = () => {
+  screen.innerText = typedValue;
 };
